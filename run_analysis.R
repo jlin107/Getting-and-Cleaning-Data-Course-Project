a5a11f071e283ad5cbb8b9de654a977b8c697088
jlin107 <jlin107@jh.edu>
2020-04-14 12:47:27 -0400

    Edit run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,45 @@
-#### 0. Download data sets ----
-# Download and unload zip file
+library(dplyr)
+
+#### 0. Download data sets. ----
 if (!file.exists("UCI HAR Dataset.zip")) {
   download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
                 destfile = "UCI HAR Dataset.zip")
 }
-
-# Store raw data in a directory called "UCI HAR Dataset"
 if (!file.exists("UCI HAR Dataset")) {
   unzip(zipfile = "UCI HAR Dataset.zip", exdir = ".")
 }
 
-#### 1. Merge train and test sets ----
+#### 1. Merge train and test data sets. ----
+train_data <- cbind(read.table("UCI HAR Dataset/train/subject_train.txt"),
+                    read.table("UCI HAR Dataset/train/y_train.txt"),
+                    read.table("UCI HAR Dataset/train/X_train.txt"))
+test_data <- cbind(read.table("UCI HAR Dataset/test/subject_test.txt"),
+                   read.table("UCI HAR Dataset/test/y_test.txt"),
+                   read.table("UCI HAR Dataset/test/X_test.txt"))
+merged_data <- rbind(train_data, test_data)
+
+#### 4. Label variables. ----
+feature_names <- read.table("UCI HAR Dataset/features.txt",
+                            colClasses = "character")
+feature_names <- feature_names[[2]]
+colnames(merged_data) <- c("subject", "activity", feature_names)
+
+#### 2. Extract mean and standard deviation data. ----
+merged_data <- merged_data[,c(1:2,grep("mean|std", colnames(merged_data)))]
+
+#### 3. Label activities. ----
+activity_names <- read.table("UCI HAR Dataset/activity_labels.txt",
+                             colClasses = "character")
+activity_names <- activity_names[[2]]
+activity_names <- tolower(activity_names)
+
+for (i in 1:6) {
+  merged_data$activity <- gsub(i, activity_names[i], merged_data$activity)
+}
+
+#### 5. Average each variable by activity and subject. ----
+for (i in colnames(merged_data)[-(1:2)]) {
+  temp <- merged_data %>%
+    group_by(subject, activity) %>%
+    summarize(mean = mean(!!sym(i)))
+}
