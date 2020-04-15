bf49eee543a308903aa346f1dc39abc4dae06547
jlin107 <jlin107@jh.edu>
2020-04-15 10:53:26 -0400

    Complete run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,6 @@
 library(dplyr)
 
-#### 0. Download data sets. ----
+#### Download data sets. ----
 if (!file.exists("UCI HAR Dataset.zip")) {
   download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
                 destfile = "UCI HAR Dataset.zip")
@@ -18,7 +18,7 @@ test_data <- cbind(read.table("UCI HAR Dataset/test/subject_test.txt"),
                    read.table("UCI HAR Dataset/test/X_test.txt"))
 merged_data <- rbind(train_data, test_data)
 
-#### 4. Label variables. ----
+#### 4. Appropriately label variables. ----
 feature_names <- read.table("UCI HAR Dataset/features.txt",
                             colClasses = "character")
 feature_names <- feature_names[[2]]
@@ -32,14 +32,25 @@ activity_names <- read.table("UCI HAR Dataset/activity_labels.txt",
                              colClasses = "character")
 activity_names <- activity_names[[2]]
 activity_names <- tolower(activity_names)
-
 for (i in 1:6) {
   merged_data$activity <- gsub(i, activity_names[i], merged_data$activity)
 }
 
 #### 5. Average each variable by activity and subject. ----
+average_data <- merged_data %>%
+  group_by(subject, activity) %>%
+  summarize() %>%
+  ungroup()
+
 for (i in colnames(merged_data)[-(1:2)]) {
-  temp <- merged_data %>%
+  average_data[[i]] <- merged_data %>%
     group_by(subject, activity) %>%
-    summarize(mean = mean(!!sym(i)))
+    summarize(mean = mean(!!sym(i))) %>%
+    ungroup() %>%
+    select(mean) %>%
+    unlist() %>%
+    as.numeric()
 }
+
+#### Write average data to file. ----
+write.table(average_data, file = "average_data.txt", row.names = FALSE)
